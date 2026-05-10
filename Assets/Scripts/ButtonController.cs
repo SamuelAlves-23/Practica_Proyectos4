bf2bcc465a7a6073c5b01617a82c012df2b5c216
ParodiaxRandom <alejandrogonzalezlozano1@gmail.com>
2026-05-10 14:07:37 +0200

    a

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("SceneTest");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+    
+}
---
+++ Assets/Scripts/HUDController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+
+public class HUDController : MonoBehaviour
+{
+    public TMP_Text criaturas;
+    public TMP_Text intrusos;
+    public GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //criaturas.text = player.criaturas;
+        //intrusos.text = player.intrusos;
+    }
+}
