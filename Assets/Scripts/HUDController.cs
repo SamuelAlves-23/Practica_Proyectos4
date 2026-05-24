0ed98ee7505be399a9aceece2b718207962d84b7
ParodiaxRandom <alejandrogonzalezlozano1@gmail.com>
2026-05-24 02:07:25 +0200

    HUD
    Lógica de restar contador al eliminar enemigos añadida

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -6,16 +6,32 @@ public class HUDController : MonoBehaviour
     public TMP_Text criaturas;
     public TMP_Text intrusos;
     public GameObject player;
+    public int criaturasR;
+    public int intrusosR;
+    public GameObject[] enemies;
+    public GameObject[] creatures;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        criaturasR = enemies.Length;
+        intrusosR = creatures.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //criaturas.text = player.criaturas;
-        //intrusos.text = player.intrusos;
+       
+    }
+
+    public void KillCreature()
+    {
+        criaturasR--;
+        criaturas.text = criaturasR.ToString();
+    }
+
+    public void KillIntruder()
+    {
+        intrusosR--;
+        intrusos.text = intrusosR.ToString();
     }
 }
